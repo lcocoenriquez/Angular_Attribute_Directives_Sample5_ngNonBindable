7f7504c0a73c216b4c50f08d1195c84ba14103c6
lcocoenriquez <lcocoenriquez@gmail.com>
2025-09-15 14:12:33 +0000

    first_commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HighlightDirective } from './highlight';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HighlightDirective],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
